9625c8cd80e51531fce24b70c4fe57964d15579b
teo.samarzija <teo.samarzija@gmail.com>
2021-01-28 22:15:21 +0100

    Added the ability to show the machine code in RecyclerView.

--- app/src/main/java/hr/ferit/teo_samarzija/picoblaze_simulator/machineCode.java
+++ app/src/main/java/hr/ferit/teo_samarzija/picoblaze_simulator/machineCode.java
@@ -4,12 +4,21 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class machineCode extends AppCompatActivity {
+  AdapterForMachineCode adapter;
+  LinearLayoutManager manager;
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.machine_code);
+    RecyclerView recyclerView=findViewById(R.id.recyclerView2);
+    adapter=new AdapterForMachineCode();
+    recyclerView.setAdapter(adapter);
+    manager=new LinearLayoutManager(this);
+    recyclerView.setLayoutManager(manager);
   }
   public void showAssemblyCode(View view) {
     Intent intent = new Intent(this, MainActivity.class);
