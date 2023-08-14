023960215f86aba9e162f829c3f82e5432740f69
Teo Samaržija <teo.samarzija@gmail.com>
2023-08-14 14:00:24 +0200

    Removed duplicate code

--- app/src/main/assets/keyword_list.js
+++ app/src/main/assets/keyword_list.js
@@ -73,71 +73,11 @@ let breakpoints = [];
 function formatAsAddress(n) {
   let ret = Math.round(n).toString(16);
   if (Math.round(n) >= 4096 || Math.round(n) < 0) {
-    alert(
-      "Some part of the compiler tried to format the number " +
-        n +
-        " as an address, which makes no sense."
-    );
+    alert("Some part of the compiler tried to format the number " + n +
+          " as an address, which makes no sense.");
     return "fff";
   }
-  while (ret.length < 3) ret = "0" + ret;
+  while (ret.length < 3)
+    ret = "0" + ret;
   return ret;
-}
-
-function formatAsByte(n) {
-  n = Math.round(n);
-  if (n < 0 || n > 255) {
-    alert(
-      "Some part of the assembler tried to format the number " +
-        n +
-        " as a byte, which makes no sense."
-    );
-    return "ff";
-  }
-  let ret = n.toString(16);
-  while (ret.length < 2) ret = "0" + ret;
-  return ret;
-}
-
-function formatAsInstruction(n) {
-  n = Math.round(n);
-  if (n < 0 || n >= 1 << 18) {
-    alert(
-      "Some part of the assembler tried to format the number " +
-        n +
-        " as a byte, which makes no sense."
-    );
-    return "ff";
-  }
-  let ret = n.toString(16);
-  while (ret.length < 5) ret = "0" + ret;
-  return ret;
-}
-
-function formatAs4bits(n) {
-  n = Math.round(n);
-  if (n < 0 || n >= 1 << 4) {
-    alert(
-      "Some part of the assembler tried to format the number " +
-        n +
-        " as a 4 bits, which makes no sense."
-    );
-    return "f";
-  }
-  let ret = n.toString(16);
-  while (ret.length < 1) ret = "0" + ret;
-  return ret;
-}
-
-function isDirective(str) {
-  if (typeof str !== "string") {
-    alert(
-      'Internal compiler error: The first argument of the "isDirective" function is not a string!'
-    );
-    return false;
-  }
-  for (const directive of preprocessor)
-    if (RegExp("^" + directive + "$", "i").test(str)) return true;
-  if (/:$/.test(str)) return true;
-  return false;
 }
